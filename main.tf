d63c5405bea50481abcd85869951832d13e5befd
Adnan <adnanshaikh11501@gmail.com>
2024-01-03 20:38:18 +0530

    Subnets & Terrafile

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 module "vpc"{
-  source        = "git::https://github.com/Adnan-110/Terraform-module-vpc.git"
-  ENV           = var.ENV
-  VPC_CIDR      = var.VPC_CIDR
+  source              = "git::https://github.com/Adnan-110/Terraform-module-vpc.git"
+  ENV                 = var.ENV
+  VPC_CIDR            = var.VPC_CIDR
+  PUBLIC_SUBNET_CIDR  = var.PUBLIC_SUBNET_CIDR
+  PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR
 }
--- variables.tf
+++ variables.tf
@@ -1,2 +1,4 @@
 variable "VPC_CIDR" {}
 variable "ENV" {}
+variable "PUBLIC_SUBNET_CIDR" {}
+variable "PRIVATE_SUBNET_CIDR" {}
