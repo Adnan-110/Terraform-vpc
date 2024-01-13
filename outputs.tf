54c3636e8bdb87448e49f49fda96f8df29b9dea8
Adnan <adnanshaikh11501@gmail.com>
2024-01-13 20:40:11 +0530

    Terraform-vpc hosted zone var comment

--- outputs.tf
+++ outputs.tf
@@ -30,16 +30,16 @@ output "DEFAULT_VPC_ID" {
     value   = module.vpc.DEFAULT_VPC_ID
 }
 
-output "PUBLIC_HOSTED_ZONE_ID" {
-    value = var.PUBLIC_HOSTED_ZONE_ID
-}
-output "PUBLIC_HOSTED_ZONE_NAME" {
-    value = var.PUBLIC_HOSTED_ZONE_NAME
-}
+# output "PUBLIC_HOSTED_ZONE_ID" {
+#     value = var.PUBLIC_HOSTED_ZONE_ID
+# }
+# output "PUBLIC_HOSTED_ZONE_NAME" {
+#     value = var.PUBLIC_HOSTED_ZONE_NAME
+# }
 
-output "PRIVATE_HOSTED_ZONE_ID" {
-    value = var.PRIVATE_HOSTED_ZONE_ID
-}
-output "PRIVATE_HOSTED_ZONE_NAME" {
-    value = var.PRIVATE_HOSTED_ZONE_NAME
-}
+# output "PRIVATE_HOSTED_ZONE_ID" {
+#     value = var.PRIVATE_HOSTED_ZONE_ID
+# }
+# output "PRIVATE_HOSTED_ZONE_NAME" {
+#     value = var.PRIVATE_HOSTED_ZONE_NAME
+# }
