9ce9fa88ae3480f520f9b16885f85974d48348f9
Adnan <adnanshaikh11501@gmail.com>
2024-01-13 20:10:10 +0530

    Terraform-vpc outputs & variables

--- variables.tf
+++ variables.tf
@@ -9,3 +9,10 @@ variable "AZ" {}
 variable "DEFAULT_VPC_CIDR" {}
 variable "DEFAULT_VPC_ID" {}
 variable "DEFAULT_VPC_RT" {}
+
+
+
+variable "PUBLIC_HOSTED_ZONE_ID" {}
+variable "PUBLIC_HOSTED_ZONE_NAME" {}
+variable "PRIVATE_HOSTED_ZONE_ID" {}
+variable "PRIVATE_HOSTED_ZONE_NAME" {}
